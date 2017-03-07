481bdd1c709ba99237edf0cdea1b70e857610985
srayabar@Office <naath.r@gmail.com>
2017-03-07 11:12:16 +0530

    started basic exercises

---
+++ PyTest/com/test/HelloWorld.py
@@ -0,0 +1,12 @@
+'''
+Created on 06-Mar-2017
+
+@author: srayabar
+'''
+print "Hello World Python!"
+print "Hello, how are you?"
+print 'this is single quoted text'
+print "what you 'are' doing ahh?"
+print 'this is "weird!"'
+print "How about /this/?"
+print "How about \this\ again?"
