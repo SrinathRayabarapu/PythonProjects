7541acc3dda390aa37db0735b40de8eba22de47c
srayabar@Office <naath.r@gmail.com>
2017-04-04 18:51:59 +0530

    moddu mokam commit1

--- PyTest/com/test/ReadingFiles.py
+++ PyTest/com/test/ReadingFiles.py
@@ -6,3 +6,8 @@ fileName = raw_input("Hey, what file you want me to open? ")
 txt = open(fileName)
 
 print "Here's your text\n", txt.read()
+
+txt.close()
+
+#below line doesn't work
+#print txt.read()
