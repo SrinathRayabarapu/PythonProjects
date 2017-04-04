9c6e6b1ce803b8d39411ab64a867c8efb938ee0b
srayabar@Office <naath.r@gmail.com>
2017-04-04 18:08:39 +0530

    moddu mokam commit

--- PyTest/com/test/Parameters.py
+++ PyTest/com/test/Parameters.py
@@ -1,3 +1,15 @@
+'''
+about command level parameters and other stuff
+'''
+
+from sys import argv
+
+fileName, param1, param2, param3 = argv
+
+print "The script called is ", fileName
+print "First arguement is ", param1
+print "Second arguement is ", param2
+print "Third arguement is ", param3
 
 
 
---
+++ PyTest/com/test/ReadingFiles.py
@@ -0,0 +1,8 @@
+'''
+about reading and writing files
+'''
+
+fileName = raw_input("Hey, what file you want me to open? ")
+txt = open(fileName)
+
+print "Here's your text\n", txt.read()
